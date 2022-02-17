45b3d89a26d1dfb47fe2e5d9816d194466c40c9e
LuisEncastin <luisencastin@gmail>
2022-02-16 20:24:44 -0600

    Problema de carga resuelto

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { useState } from 'react';
-import Background from './components/Background'
-import Settings from './components/Settings'
-import Board from './components/Board'
+
+import Background from './components/Background';
+import Board from './components/Board';
+import Settings from './components/Settings';
 
 function App() {
     const [gameOptions, setGameOptions] = useState(null);
 
-
-    const startGame = (options) => {
+    const startGame = options => {
         setGameOptions(options);
     }
 
@@ -15,9 +15,7 @@ function App() {
         <>
             <Background/>
             <h1>Memory Game</h1>
-            {!gameOptions ? 
-            (<Settings startGame={startGame}/>) : 
-            (<Board gameOptions={gameOptions}/>)}
+            {!gameOptions ? <Settings startGame={startGame}/> : <Board/>}
         </>
     );
 }
--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import useGetImages from './../../hooks/useGetImages';
+
 import styles from './Board.module.css';
 
-import useGetImages from './hooks/useGetImages'
-
-const Board = (gameOptions) => {
-    const images = useGetImages(gameOptions);
+const Board = ({gameOptions}) => {
+    const images = useGetImages(gameOptions); //Si quieres board en el mismo espacio, vamos a tener que regresar esto a app.
     console.log({images});
 
     return <p>Board</p>;
